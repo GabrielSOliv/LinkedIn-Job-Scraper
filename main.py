0ab9052176b4c614dd0033ac78525c8a6bd74d93
GabrielSOliv <gabriel.sv.oliver@gmail.com>
2024-09-14 14:18:35 -0300

    catch links

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
-
 search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys('site:linkedin.com/jobs/view ("Python" OR "Desenvolvedor Python" OR "Desenvolvedor Jr Python") ' \
        '(intext:"remoto" OR intext:"Remote") ' \
@@ -20,32 +19,32 @@ search_box.send_keys('site:linkedin.com/jobs/view ("Python" OR "Desenvolvedor Py
        '(intext:"VBA" OR intext:"Python" OR intext:"SQL" OR intext:"Inglês" OR intext:"Excel" OR intext:"Power BI")')
 search_box.send_keys(Keys.RETURN)
 
-time.sleep(35)
+time.sleep(3)
 
 tool_btn = driver.find_element(By.XPATH, '//*[@id="hdtb-tls"]')
 tool_btn.send_keys(Keys.RETURN)
 
-time.sleep(5)
+time.sleep(3)
 
 
 date_picker = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[5]/div/div/div[1]/span[2]/g-popup/div[1]/div')
 date_picker.click()
 
-time.sleep(5)
+time.sleep(3)
 
 last_month = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[5]/div/div/div[1]/span[2]/g-popup/div[2]/g-menu/g-menu-item[5]/div/a')
 last_month.click()
 
-time.sleep(5)
+time.sleep(3)
 
 
 links = []
-results = driver.find_elements(By.XPATH, '//*[@id="center_col"]')[:10]
+results = driver.find_elements(By.XPATH, '//*[@id="rso"]//a')[:10]
 for result in results:
     links.append(result.get_attribute('href'))
 
 df = pd.DataFrame(links, columns=['Links'])
-df.to_excel('resultados_google.xlsx', index=False)
+df.to_excel('job_links.xlsx', index=False)
 
 
 driver.quit()
